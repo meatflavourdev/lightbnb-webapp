6145bff8384c2eb88200e5796b1f8f4872bd73bc
Jeremy Felix D <me@jeremyfelix.com>
2020-11-26 22:30:50 -0800

    Implement getAllProperties()

--- server/database.js
+++ server/database.js
@@ -79,12 +79,11 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+  return pool
+    .query('SELECT * FROM properties LIMIT $1', [limit])
+    .then((res) => res.rows)
+    .catch(err => console.error('Error executing query', err.stack));
+};
 exports.getAllProperties = getAllProperties;
 
 
